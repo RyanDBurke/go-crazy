39966ad4edaac5ec4b69617d3926a8865d5dd006
Ryan Burke <ryanburke111@hotmail.com>
2021-03-06 22:16:58 -0500

    channels fixed

--- concurrency/binary_tree.go
+++ concurrency/binary_tree.go
@@ -16,19 +16,27 @@ func Walk(t *tree.Tree, ch chan int) {
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
-	ch1 := make(chan int, 10)
-	ch2 := make(chan int, 10)
+
+	const CAP = 10
+	
+	ch1 := make(chan int, CAP)
+	ch2 := make(chan int, CAP)
 
 	go func() {
 		Walk(t1, ch1)
-		Walk(t2, ch2)
+	 	Walk(t2, ch2)
 	}()
 
 	defer close(ch1)
 	defer close(ch2)
 
-	for i := range ch1 {
-		fmt.Println(i)
+	for i := 0; i < CAP; i++ {
+		x := <-ch1
+		y := <-ch2
+		fmt.Println("x: ", x, ", y: ", y)
+		if x != y {
+			return false
+		}
 	}
 
 	return true
@@ -46,6 +54,10 @@ func main() {
 		Walk(tree.New(1), ch)
 	}()
 
-	res := Same(tree.New(1), tree.New(1))
-	fmt.Println(res)
+	res := Same(tree.New(2), tree.New(2))
+	if res {
+		fmt.Println("They are equal")
+	} else {
+		fmt.Println("They are NOT equal")
+	}
 }
