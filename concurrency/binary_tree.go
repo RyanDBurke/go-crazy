ac135828a8f46119050cd7a04eca57b5214912cf
Ryan Burke <ryanburke111@hotmail.com>
2021-03-06 21:50:19 -0500

    error on chan

--- concurrency/binary_tree.go
+++ concurrency/binary_tree.go
@@ -8,28 +8,44 @@ import (
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
 func Walk(t *tree.Tree, ch chan int) {
-	if t.Left != nil {
-        Walk(t.Left, ch)
-    }
-    ch <- t.Value
-    if t.Right != nil {
-        Walk(t.Right, ch)
-    }
+	if t.Left != nil	{ Walk(t.Left, ch) }
+	ch <- t.Value
+    if t.Right != nil 	{ Walk(t.Right, ch) }
 }
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int, 10)
+	ch2 := make(chan int, 10)
+
+	go func() {
+		Walk(t1, ch1)
+		Walk(t2, ch2)
+	}()
+
+	defer close(ch1)
+	defer close(ch2)
+
+	for i := range ch1 {
+		fmt.Println(i)
+	}
+
 	return true
 }
 
+
 func main() {
 
-	// https://tour.golang.org/concurrency/8
-
 	// create channel
-	ch := make(chan int, 20)
+	ch := make(chan int)
 
 	// run goroutine
-	go Walk(tree.New(1), ch)
+	go func() {
+		defer close(ch)
+		Walk(tree.New(1), ch)
+	}()
+
+	res := Same(tree.New(1), tree.New(1))
+	fmt.Println(res)
 }
